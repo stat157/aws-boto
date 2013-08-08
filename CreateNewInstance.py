a0c937256a1598f8f0f03e789cc8706522a1a775
Aaron Culich <aculich@eecs.berkeley.edu>
2013-08-08 15:12:03 -0700

    added missing parenthesis to connect_to_region() call

--- CreateNewInstance.py
+++ CreateNewInstance.py
@@ -1,7 +1,7 @@
 import boto.ec2
 conn = boto.ec2.connect_to_region("us-west-1",
     aws_access_key_id='YOUR_KEY_ID_HERE',
-    aws_secret_access_key='YOUR_SECRET_ACCESS_KEY_HERE'
+    aws_secret_access_key='YOUR_SECRET_ACCESS_KEY_HERE')
 
 conn.run_instances(
     'ami-d383af96',
