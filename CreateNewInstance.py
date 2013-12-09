08450c0dede9ac225b54e31e8aa417f5927431a1
tristan <tao.tristan@gmail.com>
2013-12-08 20:47:53 -0800

    added error msg report

--- CreateNewInstance.py
+++ CreateNewInstance.py
@@ -1,5 +1,11 @@
 import boto.ec2
+import sys
 conn = boto.ec2.connect_to_region("us-west-2")
+
+if conn is None:
+	print "ERROR: Could not connect. Perhaps try connecting to one of the following regions"
+	print boto.ec2.regions()
+	sys.exit(1)
 
 conn.run_instances(
     'ami-ace67f9c',
